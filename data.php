3202940057f7283a0b281cdd83d6a65a33024c1c
Elhias34 <epharra@gmail.com>
2024-06-26 09:19:21 -0400

    Add files via upload

---
+++ data.php
@@ -0,0 +1,11 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$mensaje = $_POST['mensaje'];
+
+echo "Este mensaje fue enviado desde php";
+
+echo "<p>el nombre es $nombre";
+echo "<p>el mensaje es $mensaje";
+
+?>
